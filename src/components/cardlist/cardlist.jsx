d3a717481e453f6669ac99321db4b05e58eddd8f
Your Name <you@example.com>
2024-01-24 12:16:39 +0300

    dfsg

---
+++ src/components/cardlist/cardlist.jsx
@@ -0,0 +1,12 @@
+import Card from '../card/card';
+import './cardlist.scss'
+
+function Cardlist() {
+    return <div className="card-list">
+        <div className="container">
+            <Card/>
+        </div>
+    </div>;
+}
+
+export default Cardlist;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,32 @@
+import './header.scss'
+
+function Header() {
+    return <header>
+        <div className="container">
+            <h1>Harry Potter</h1>
+            <h3>View all characters from the Harry Potter universe</h3>
+        </div>
+
+        <div className="inputs">
+            <form action=''>
+                <div className="name">
+                    <label htmlFor='name'>Имя</label>
+                    <input type="text" placeholder='Гермиона' id='name'></input>
+                </div>
+
+                <div className="school">
+                    <label htmlFor='school'>Школа</label>
+                    <select name='' id='school'>
+                        <option value="0">Выберите</option>
+                        <option value="1">Гриффиндор</option>
+                        <option value="2">Слизерин</option>
+                        <option value="3">Когтевран</option>
+                        <option value="4">Пуффендуй</option>
+                    </select>
+                </div>
+            </form>
+        </div>
+    </header>;
+}
+
+export default Header;
